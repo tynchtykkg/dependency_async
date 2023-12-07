befd9e014e0197f1195e162891439b909ac9b14e
tynchtykkg <tynchtyk.estebesuulu@gmail.com>
2023-12-07 21:59:28 +0600

    first commit

---
+++ bin/dependency_async.dart
@@ -0,0 +1,5 @@
+import 'package:dependency_async/dependency_async.dart' as dependency_async;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dependency_async.calculate()}!');
+}
---
+++ test/dependency_async_test.dart
@@ -0,0 +1,8 @@
+import 'package:dependency_async/dependency_async.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
